5d0ab72baaca3ba3e7eeea0d89d2936d7f9c2827
Louis Constantin <20269762+lconstan@users.noreply.github.com>
2024-12-06 10:25:24 +0100

    Update getting ip address for linux

--- CameraVideoRecorder/Camera/CameraIpService.cs
+++ CameraVideoRecorder/Camera/CameraIpService.cs
@@ -1,24 +1,30 @@
 ﻿using CameraVideoRecorder.Arguments;
 using System.Net.NetworkInformation;
 using System.Net;
+using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
 
 namespace CameraVideoRecorder.Camera
 {
     internal class CameraIpService : ICameraIpService
     {
         private readonly ICameraRecorderArgumentProvider _argumentProvider;
+        private readonly ILogger<CameraIpService> _logger;
         private const string _cameraMacAddress = "04-17-b6-01-39-c5";
 
         private string _cameraIp;
 
-        public CameraIpService(ICameraRecorderArgumentProvider argumentProvider)
+        public CameraIpService(ICameraRecorderArgumentProvider argumentProvider, ILogger<CameraIpService> logger)
         {
             _argumentProvider = argumentProvider;
+            _logger = logger;
         }
 
         public bool CanPingCamera()
         {
             _cameraIp = GetCameraIpAddress();
+
+            _logger.LogInformation("Found ip {ip}", _cameraIp);
 
             if (string.IsNullOrEmpty(_cameraIp))
             {
@@ -37,22 +43,44 @@ namespace CameraVideoRecorder.Camera
 
         private string GetCameraIpAddress()
         {
-            using System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
-            pProcess.StartInfo.FileName = "arp";
-            pProcess.StartInfo.Arguments = "-a";
-            pProcess.StartInfo.UseShellExecute = false;
-            pProcess.StartInfo.RedirectStandardOutput = true;
-            pProcess.StartInfo.CreateNoWindow = true;
-            pProcess.Start();
-            string strOutput = pProcess.StandardOutput.ReadToEnd();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return GetIpAddressWindows();
+            }
+
+            return GetIpAddressLinux();
+        }
+
+        private string GetIpAddressLinux()
+        {
+            string strOutput = RunProcessAndGetOutput("nmap", "-sn 192.168.1.0/24");
 
             using var sr = new StringReader(strOutput);
             string line;
 
             while ((line = sr.ReadLine()) != null)
             {
+                if (!line.Contains("Indoorcam"))
+                {
+                    continue;
+                }
+
+                return line.Replace("Nmap scan report for Indoorcam (", "").Replace(")", "");
+            }
+
+            return null;
+        }
+
+        private string GetIpAddressWindows()
+        {
+            string strOutput = RunProcessAndGetOutput("arp", "-a");
+            using var sr = new StringReader(strOutput);
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
                 line = line.Trim();
-                
+
                 if (!line.StartsWith("192.168"))
                 {
                     continue;
@@ -60,23 +88,36 @@ namespace CameraVideoRecorder.Camera
 
                 if (line.Contains(_cameraMacAddress))
                 {
-                    return ParseIpAdress(line);
+                    int index = 0;
+
+                    while (char.IsDigit(line[index]) || line[index] == '.')
+                    {
+                        index++;
+                    }
+
+                    return line.Substring(0, index);
                 }
             }
 
             return null;
         }
 
-        private string ParseIpAdress(string line)
+        private static string RunProcessAndGetOutput(string command, string parameter = null)
         {
-            int index = 0;
+            using var pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = command;
 
-            while (char.IsDigit(line[index]) || line[index] == '.')
-            { 
-                index++;
+            if (parameter != null)
+            {
+                pProcess.StartInfo.Arguments = parameter;
             }
 
-            return line.Substring(0, index);
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.Start();
+
+            return pProcess.StandardOutput.ReadToEnd();
         }
 
         public string GetCameraIp()
